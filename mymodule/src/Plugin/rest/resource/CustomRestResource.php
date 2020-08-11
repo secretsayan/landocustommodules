0872ac7f6f6b43a4a805bc53e9e05c65b9267690
Sayan Goswami <secretsayan@gmail.com>
2020-08-11 08:12:50 +0100

    Docusign + Rest Resource

--- mymodule/src/Plugin/rest/resource/CustomRestResource.php
+++ mymodule/src/Plugin/rest/resource/CustomRestResource.php
@@ -24,77 +24,80 @@ use Psr\Log\LoggerInterface;
  * )
  */
 class CustomRestResource extends ResourceBase
- {
-  /**
-   * A current user instance.
-   *
-   * @var \Drupal\Core\Session\AccountProxyInterface
-   */
-  protected $currentUser;
+{
+    /**
+     * A current user instance.
+     *
+     * @var \Drupal\Core\Session\AccountProxyInterface
+     */
+    protected $currentUser;
 
-  /**
-   * Constructs a Drupal\rest\Plugin\ResourceBase object.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param array $serializer_formats
-   *   The available serialization formats.
-   * @param \Psr\Log\LoggerInterface $logger
-   *   A logger instance.
-   * @param \Drupal\Core\Session\AccountProxyInterface $current_user
-   *   A current user instance.
-   */
-  public function __construct(
-    array $configuration,
-    $plugin_id,
-    $plugin_definition,
-    array $serializer_formats,
-    LoggerInterface $logger,
-    AccountProxyInterface $current_user) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+    /**
+     * Constructs a Drupal\rest\Plugin\ResourceBase object.
+     *
+     * @param array                                      $configuration
+     *   A configuration array containing information about the plugin instance.
+     * @param string                                     $plugin_id
+     *   The plugin_id for the plugin instance.
+     * @param mixed                                      $plugin_definition
+     *   The plugin implementation definition.
+     * @param array                                      $serializer_formats
+     *   The available serialization formats.
+     * @param \Psr\Log\LoggerInterface                   $logger
+     *   A logger instance.
+     * @param \Drupal\Core\Session\AccountProxyInterface $current_user
+     *   A current user instance.
+     */
+    public function __construct(
+        array $configuration,
+        $plugin_id,
+        $plugin_definition,
+        array $serializer_formats,
+        LoggerInterface $logger,
+        AccountProxyInterface $current_user
+    ) {
+        parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
 
-    $this->currentUser = $current_user;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->getParameter('serializer.formats'),
-      $container->get('logger.factory')->get('mymodule_rest'),
-      $container->get('current_user')
-    );
-  }
-
-  /**
-   * Responds to POST requests.
-   *
-   * Returns a list of bundles for specified entity.
-   *
-   * @param $node_type
-   * @param $data
-   * @return \Drupal\rest\ResourceResponse Throws exception expected.
-   * Throws exception expected.
-   */
-  public function post($data) {
-
-    // You must to implement the logic of your REST Resource here.
-    // Use current user after pass authentication to validate access.
-    if (!$this->currentUser->hasPermission('access content')) {
-      throw new AccessDeniedHttpException();
+        $this->currentUser = $current_user;
     }
 
+    /**
+     * {@inheritdoc}
+     */
+    public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
+    {
+        return new static(
+            $configuration,
+            $plugin_id,
+            $plugin_definition,
+            $container->getParameter('serializer.formats'),
+            $container->get('logger.factory')->get('mymodule_rest'),
+            $container->get('current_user')
+        );
+    }
 
-    $node = Node::create(
-        array(
+    /**
+     * Responds to POST requests.
+     *
+     * Returns a list of bundles for specified entity.
+     *
+     * @param  $node_type
+     * @param  $data
+     * @return \Drupal\rest\ResourceResponse Throws exception expected.
+     * Throws exception expected.
+     */
+    public function post($data)
+    {
+
+        // You must to implement the logic of your REST Resource here.
+        // Use current user after pass authentication to validate access.
+        if (!$this->currentUser->hasPermission('access content')) {
+            throw new AccessDeniedHttpException();
+        }
+
+
+        $node = Node::create(
+            array(
             'type' => $data->type->target_id,
             'title' => $data->title->value,
             'body' => [
@@ -102,14 +105,15 @@ class CustomRestResource extends ResourceBase
               'value' => $data->body->value,
               'format' => 'full_html',
             ],
-          )
-    );
-    $node->save();
-    return new ResourceResponse($node);
-  }
+            )
+        );
+        $node->save();
+        return new ResourceResponse($node);
+    }
 
-  public function get($node){
-    return "Sayan";
-  }
+    public function get($node)
+    {
+        return "Sayan";
+    }
 
 }
